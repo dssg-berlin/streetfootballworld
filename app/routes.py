55509f36121fe96162346b063fa18fa127ecf842
HndrkMkt <hendrik.makait@nordakademie.de>
2015-10-24 21:18:00 +0200

    combine it all

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, request
 from forms import KeywordForm
+import json
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -9,11 +10,8 @@ app.config.from_object('config')
 def index():
     form = KeywordForm()
     keyword = request.form.get('keyword', '')
-    posts = []
-    for i in range(1, 6):
-        posts.append({"relevance": 1. / i,
-                      "sentiment": 2.5 - i,
-                      "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus erat ac tristique consequat. Nullam justo nibh, iaculis eget magna vehicula, sodales vehicula augue. Quisque quis aliquam diam, non aliquam mauris. Vivamus interdum, mauris vitae tincidunt tempor, sapien odio lobortis leo, at iaculis nisl ligula sed ligula. Sed a mi suscipit dui cursus varius non a elit. Nulla facilisi. Pellentesque mollis ipsum sit amet libero euismod, ut scelerisque arcu lacinia. Nulla viverra sapien lectus."})
+    posts = json.load(open("./static/resources/topics.json"))
+    posts = posts[0]['posts']
     return render_template('index.html', form=form, keyword=keyword, posts=posts)
 
 
