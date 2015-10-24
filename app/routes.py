0db51561624f9a6d91accf0d7236e41fea8749d2
HndrkMkt <hendrik.makait@nordakademie.de>
2015-10-24 17:31:01 +0200

    add form

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
+from forms import KeywordForm
+
 
 app = Flask(__name__)
+app.config.from_object('config')
 
-@app.route('/')
-def home():
-  return render_template('home.html')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+  form = KeywordForm()
+  if form.validate_on_submit():
+    keyword = request.form['keyword']
+    return redirect('/', form=form, keyword=keyword)
+  return render_template('index.html', form=form)
 
 if __name__ == '__main__':
   app.run(debug=True)
