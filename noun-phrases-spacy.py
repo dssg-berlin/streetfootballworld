db3046a23ea971cc954ecdc2ec2fe39062f104e6
HndrkMkt <hendrik.makait@nordakademie.de>
2015-10-24 14:58:23 +0200

    apply lemmatization and use all emails

--- noun-phrases-spacy.py
+++ noun-phrases-spacy.py
@@ -2,15 +2,15 @@ import spacy.en
 from spacy.parts_of_speech import NOUN
 from collections import defaultdict
 
-dataDir = "/Users/hendster/streetfootballworld/DSSG_unleashfootball/TheTranslatedPosts"
-text = open(dataDir + "/trans_post1.txt").read()
-print(text)
+dataDir = "/Users/hendster/streetfootballworld/DSSG_unleashfootball/TheRevisedTranslatedPosts"
+text = open(dataDir + "/all_posts_concatenated.txt", encoding="ISO-8859-2").read().lower().
 
 nlp = spacy.en.English()
 tokens = nlp(text, tag=True, parse=False)
 noun_freq = defaultdict(int)
 for token in tokens:
     if token.pos == NOUN:
-        noun_freq[token.string] += 1
+        noun_freq[token.lemma_] += 1
 print(noun_freq)
 
+sorted(noun_freq.items(), key= lambda x: x[1], reverse=True)
